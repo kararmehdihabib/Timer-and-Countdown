e74ecc80e9f04b87377643880d728eae0522b1c9
kararmehdi <karar.habib2@metropolia.fi>
2016-10-19 17:47:52 +0300

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+
+//App CSS
+require('style!css!sass!applicationStyles')
+
+// Load Foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation;
+
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+       <Route path="/" component={Main}>
+        
+        </Route>
+        
+       
+  </Router>,
+  document.getElementById('app')
+);
---
+++ app/components/Main.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var Nav = require('Nav');
+
+var Main = React.createClass({
+    render:function(){
+        return(
+            <div>
+               
+                <div >
+                    <div>
+                        <Nav/>
+                        <p className="huta">Main.jsx Rendered</p>
+                    {this.props.children}
+                    
+                    </div>
+                
+                </div>
+               
+                
+               
+            </div>
+        );
+    }
+});
+
+module.exports = Main;
